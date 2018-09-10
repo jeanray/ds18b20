8b17c56354b22e9b73567035beebfceafdc52876
Jean Ødegård <jean.oedegaard@gmail.com>
2018-09-10 23:13:54 +0200

    Add project files.

---
+++ ds18b20-temp_reader/ds18b20-temp_reader.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ds18b20_temp_reader.ino
+ Created:	9/10/2018 11:13:48 PM
+ Author:	jeano
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
